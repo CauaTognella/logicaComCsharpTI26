bce8a048dac6ce763edc3f9e74682d534841bb68
CauaTognella <condlost967@gmail.com>
2025-03-26 11:02:29 -0300

    calculo_de_idade
    1º programa em C#

---
+++ calculo_de_idd/calculo_de_idd/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculo_de_idd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+           
+        {
+            int ano_nascimento, ano_atual, Idade;
+           //int decalara a variavel inteiro 
+            Console.WriteLine("Digite o ano do seu nascimento: ");
+            //console.weite =escreva 
+             ano_nascimento = int.Parse(Console.ReadLine());     
+            
+              Console.WriteLine("Digite o ano atual: ");
+               ano_atual = int.Parse(Console.ReadLine());
+            //int perse converte texto em numero 
+                Idade = ano_atual - ano_nascimento;
+                  Console.WriteLine("Sua Idade é " + Idade + " anos");
+        }
+    }
+}
