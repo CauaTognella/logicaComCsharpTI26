9c0300785b25e6dbffffb607dc3ded5c225cb8a1
CauaTognella <condlost967@gmail.com>
2025-03-26 11:48:26 -0300

    peixe e qual dos dois
    peixe e qual dos dois
    salvei tudo numa pasta so sem querer

---
+++ peixeriadokjapofp/peixeriadokjapofp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace peixeriadokjapofp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double peso, sobra;
+            Console.WriteLine("digite o peso do peixe");
+             peso = double.Parse(Console.ReadLine());
+              sobra = (peso - 50) * 4;
+            Console.WriteLine(sobra);
+        }
+    }
+}
---
+++ qual dos dois numeros é maior/qual dos dois numeros é maior/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qual_dos_dois_numeros_é_maior
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero1, numero2, total;
+
+
+                    Console.WriteLine("digite o primeiro numero desejado ");
+                   numero1 = Convert.ToDouble(Console.ReadLine());
+                 Console.WriteLine("digite o primeiro numero desejado "); ;
+                numero2 = Convert.ToDouble(Console.ReadLine());
+
+              if(numero1 > numero2)
+                Console.WriteLine("nemero 1 é maior que numero 2");
+                 else{ Console.WriteLine("numero 2 é maior que numero 1");
+            
+            }
+        }
+    }
+}
