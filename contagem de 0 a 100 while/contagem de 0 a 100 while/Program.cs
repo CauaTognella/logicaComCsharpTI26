1923808c4a0ee8ef4b6d027d90a00bfc6ebf568f
CauaTognella <condlost967@gmail.com>
2025-03-27 09:36:17 -0300

    de 0 a 1000 usando o while
    estudos de while

---
+++ contagem de 0 a 100 while/contagem de 0 a 100 while/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contagem_de_0_a_100_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+
+            //WHILE = enquato 
+            while (contador < 100) {
+                contador = contador + 2;
+                Console.WriteLine(" " + contador + "\n");
+            }
+        }
+    }
+  }
