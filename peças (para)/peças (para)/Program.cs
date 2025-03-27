2c11efdf60e56944619efece7737c49e01d28274
CauaTognella <condlost967@gmail.com>
2025-03-27 11:11:34 -0300

    peças da fabrica
    reavaliar

---
+++ peças (para)/peças (para)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace peças__para_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pecas;
+            int total;
+            int resultado;
+            Console.Write("Quantidades de peças armazenadas: ");
+            pecas = int.Parse(Console.ReadLine());
+            for (total = 400; pecas < 400; pecas++)
+            {
+                resultado = total - pecas;
+                Console.Write("O total de peças descartadas foram: ", resultado);
+
+            }
+        }
+    }
+}
