2965f5cf5531bb497f62b4f6cc7ff69efd754f79
CauaTognella <condlost967@gmail.com>
2025-03-27 09:24:22 -0300

    contador de 0 a 100
    contem anotaçoes para estudo

---
+++ contador de 1 a 100/contador de 1 a 100/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contador_de_1_a_100
+{
+    internal class Program
+    {//CONTADOR DE 0 A 100
+        static void Main(string[] args)
+        {
+            int contador;
+
+            // PARA = for
+            for (contador = 0; contador < 101; contador = contador + 2)
+            {   // Console.Write é usado como escreva se vc adicionar um line "Console.Writeline" ele fica espassado bunitinho um embaixo do outro ou sa o "\n"
+                Console.WriteLine(" " + contador);
+            }
+        }
+    }
+}
