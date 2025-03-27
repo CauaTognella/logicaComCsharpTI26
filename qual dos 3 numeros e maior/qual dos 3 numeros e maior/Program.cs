d1293c210acb0675c6b5e0d65dd677f0581992c8
CauaTognella <condlost967@gmail.com>
2025-03-27 11:36:18 -0300

    3 numeros

---
+++ qual dos 3 numeros e maior/qual dos 3 numeros e maior/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qual_dos_3_numeros_e_maior
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+
+            Console.Write("escreva um numero ");
+             num1 = int.Parse(Console.ReadLine());
+              Console.Write("escreva um numero ");
+                num2 = int.Parse(Console.ReadLine());
+                 Console.Write("escreva um numero ");
+                   num3 = int.Parse(Console.ReadLine());
+            if (num1 < num2 && num1 < num3) {
+                Console.Write("o menor numero é o numero: " + num1);
+            }
+            if (num2 < num1 && num2 < num3) {
+                Console.Write("o menor numero é o numero: ", num2);
+            }
+            if (num3 < num2 && num3 < num1) {
+                Console.Write("o menor numero é o numero: ", num3);
+            }
+        }
+    }
+}
---
+++ qual dos 3 numeros e maior/qual dos 3 numeros e maior/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("qual dos 3 numeros e maior")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("qual dos 3 numeros e maior")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("a9d1dd33-167a-4da9-96da-1ba4bb08742d")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
