96b46e0baa9fc7d192f70cdd66159bc881eba61e
CauaTognella <condlost967@gmail.com>
2025-03-27 10:40:21 -0300

    numero de 0 a 100 (for)
    estudar

---
+++ exercicio 1 numero de 0 a 100 par/exercicio 1 numero de 0 a 100 par/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_1_numero_de_0_a_100_par
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////mostre numeros PARES de 0 a 100 (for)
+
+
+            int contador;
+
+            for (contador = 0; contador < 101; contador = contador + 2)
+            {
+                Console.WriteLine(" " + contador );
+            }
+        }
+    }
+}
+  
