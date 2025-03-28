8765790cd05a2fc68f27c456296ecc14fe797c18
CauaTognella <condlost967@gmail.com>
2025-03-28 10:48:48 -0300

    dia da semana

---
+++ Dia da semana/Dia da semana/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dia_da_semana
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int diaDamana;
+
+            Console.Write("Digite o numero da semana: ");
+            diaDamana = int.Parse(Console.ReadLine());
+
+            if (diaDamana > 7)
+            {
+                Console.Write("Numero da semana invalida");
+            }
+            else if (diaDamana == 1 || diaDamana == 6)
+            {
+
+                Console.Write("Final de semana");
+            } else { 
+                Console.Write("Dia da semana normal");
+
+               }
+        }
+    }
+}
+    
+
---
+++ Dia da semana/Dia da semana/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Dia da semana")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Dia da semana")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("b6bc2041-2c2f-4d7e-a665-7a1a9deaf18a")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
