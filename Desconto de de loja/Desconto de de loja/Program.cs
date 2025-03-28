2ff5fa7c8f9316149ebbf3fcaf27aa7d789f52a9
CauaTognella <condlost967@gmail.com>
2025-03-28 09:14:43 -0300

    Desconto de loja

---
+++ Desconto de de loja/Desconto de de loja/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desconto_de_de_loja
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double precoProduto, descontoPreco;
+
+
+            Console.Write("Digite o preço do produto: ");
+    precoProduto = Double.Parse(Console.ReadLine());
+
+    if(precoProduto > 100)
+    {
+                descontoPreco = precoProduto * 0.10;
+                Console.Write(descontoPreco + " reais de desconto");
+    }
+           else
+    {
+                descontoPreco = precoProduto * 0.5;
+                Console.Write(descontoPreco + " reais de desconto");
+    }
+
+        }
+    }
+}
+    
+
+
+
