3c64447281e173db8543b2f279f197ad0f6c71c7
CauaTognella <condlost967@gmail.com>
2025-03-28 09:50:17 -0300

    idade para dirigir

---
+++ idade para dirigir/idade para dirigir/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace idade_para_dirigir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+
+    Console.Write("Digite a idade para obter a carteira de motorista");
+    idade = int.Parse(args[0]);
+
+    if(idade < 17)
+    {
+                Console.Write("NÃ£o pode dirigir");
+    }
+            else
+    {
+                Console.Write("Pode dirigir");
+    }
+        }
+    }
+}
+    
+
