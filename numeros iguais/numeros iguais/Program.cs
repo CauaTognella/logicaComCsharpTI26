bc14daf5c1a16f20ff38be3cd953aaf9199563e8
CauaTognella <condlost967@gmail.com>
2025-03-28 11:09:24 -0300

    Numeros iguais

---
+++ numeros iguais/numeros iguais/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numeros_iguais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double num1, num2;
+
+    Console.Write("Digite um número: ");
+    num1 = Double.Parse(Console.ReadLine());
+            Console.Write("Digite outro número: ");
+    num2 = Double.Parse(Console.ReadLine());
+
+            if (num1 == num2) {
+                Console.WriteLine("Números iguais");
+            }
+            else {
+                Console.WriteLine("Esse números não são iguais");
+
+        }
+        }
+    }
+}
