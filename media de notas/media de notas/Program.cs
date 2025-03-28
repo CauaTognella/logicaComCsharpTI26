36c2e68afb34dc58ebd73325be0c9e036614b415
CauaTognella <condlost967@gmail.com>
2025-03-28 10:56:26 -0300

    media de notas

---
+++ media de notas/media de notas/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace media_de_notas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double nota1, nota2, nota3, nota4, media
+                ;
+            Console.Write("Digite a nota 1: ");
+            nota1 = Double.Parse(args[0]);
+
+            Console.Write("Digite a nota 2: ");
+            nota2 = Double.Parse(args[1]);
+
+            Console.Write("Digite a nota 3: ");
+            nota3 = Double.Parse(args[2]);
+
+            Console.Write("Digite a nota 4: ");
+            nota4 = Double.Parse(args[3]);
+
+            media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+            if (media >= 7)
+            {
+                Console.Write("Aprovado com mÃŠdia: ", media);
+            }
+            else
+            {
+                Console.Write("reprovado com mÃŠdia: ", media);
+            }
+        }
+    }
+}
