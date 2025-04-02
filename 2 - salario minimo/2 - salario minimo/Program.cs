1e6b27e58b1782c23cbc637bbf455dae0ea2c5d6
CauaTognella <condlost967@gmail.com>
2025-04-02 09:25:09 -0300

    3 - salario minimo

---
+++ 2 - salario minimo/2 - salario minimo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2___salario_minimo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*2-	Faça um algoritmo que leia o valor do salário mínimo e o valor do salário de um usuário, calcule quantos salários mínimos esse 
+            usuário ganha e imprima na tela o resultado. (Base para o Salário mínimo R$ 1.518,00).*/
+
+            double sla, slam, qnt;
+            Console.WriteLine("digite seu salario: ");
+             sla = double.Parse (Console.ReadLine());
+              slam = 1518;
+               qnt = sla / slam;
+                 Console.WriteLine("sua quantidade de salarios minimos é: "+ qnt);
+
+        }
+    }
+}
