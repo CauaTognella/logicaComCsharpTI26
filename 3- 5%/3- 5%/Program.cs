39a1578945ad5804053ec23de28d2dfb026a738e
CauaTognella <condlost967@gmail.com>
2025-04-03 11:24:39 -0300

    reajuste de 5%

--- 3- 5%/3- 5%/Program.cs
+++ 3- 5%/3- 5%/Program.cs
@@ -13,7 +13,23 @@ namespace _3__5_
             /* Faça um algoritmo que leia um valor qualquer e imprima na tela com um reajuste de 5 %.*/
 
             double valor, total, tt;
-            Console.WriteLine("escreva o numero que deseja saber o reajuste de 5%: ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow
+                ;
+            Console.WriteLine(@"
+      /$$$$$$$  /$$$$$$$$  /$$$$$$     /$$$$$ /$$   /$$  /$$$$$$  /$$$$$$$$ /$$$$$$$$       /$$$$$$$  /$$$$$$$$       /$$$$$$$ /$$   /$$
+     | $$__  $$| $$_____/ /$$__  $$   |__  $$| $$  | $$ /$$__  $$|__  $$__/| $$_____/      | $$__  $$| $$_____/      | $$____/|__/  /$$/
+     | $$  \ $$| $$      | $$  \ $$      | $$| $$  | $$| $$  \__/   | $$   | $$            | $$  \ $$| $$            | $$          /$$/ 
+     | $$$$$$$/| $$$$$   | $$$$$$$$      | $$| $$  | $$|  $$$$$$    | $$   | $$$$$         | $$  | $$| $$$$$         | $$$$$$$    /$$/  
+     | $$__  $$| $$__/   | $$__  $$ /$$  | $$| $$  | $$ \____  $$   | $$   | $$__/         | $$  | $$| $$__/         |_____  $$  /$$/   
+     | $$  \ $$| $$      | $$  | $$| $$  | $$| $$  | $$ /$$  \ $$   | $$   | $$            | $$  | $$| $$             /$$  \ $$ /$$/    
+     | $$  | $$| $$$$$$$$| $$  | $$|  $$$$$$/|  $$$$$$/|  $$$$$$/   | $$   | $$$$$$$$      | $$$$$$$/| $$$$$$$$      |  $$$$$$//$$/  /$$
+     |__/  |__/|________/|__/  |__/ \______/  \______/  \______/    |__/   |________/      |_______/ |________/       \______/|__/  |__/
+                                                                                                                                   
+                                                                                                                                   
+                                                                                                                                   
+");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+             Console.WriteLine("escreva o numero que deseja saber o reajuste de 5%: ");
                 valor = double.Parse(Console.ReadLine());
                  total = 0.05 * valor;
                   tt = total + valor;
