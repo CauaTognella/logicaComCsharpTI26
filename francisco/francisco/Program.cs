a66ec29e4e5f5048e232d10e612b359be84b37ce
CauaTognella <condlost967@gmail.com>
2025-04-03 11:46:20 -0300

    francisco

---
+++ francisco/francisco/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace francisco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double francisco, sara,ano = 0;
+            francisco = 1.50;
+                sara = 1.10;
+            while (francisco > sara 
+                ; ano++) {
+        }
+    }
+}
